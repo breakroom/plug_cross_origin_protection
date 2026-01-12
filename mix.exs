e50d01f5ae2be70d2f87d54cef9021e6f13a032e
Tom Taylor <tom@tomtaylor.co.uk>
2026-01-12 16:31:42 +0000

    Bump to 0.2.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule PlugCrossOriginProtection.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.2.0"
   @source_url "https://github.com/breakroom/plug_cross_origin_protection"
 
   def project do
